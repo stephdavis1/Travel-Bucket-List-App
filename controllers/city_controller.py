4f59fc7faa98c2dcd9d81672c6286665196aec61
Steph Davis <stephdavisdesign@hotmail.com>
2020-11-30 11:36:46 +0000

    city controller updated

--- controllers/city_controller.py
+++ controllers/city_controller.py
@@ -41,6 +41,34 @@ def show_city(id):
     return render_template ("books/show.html", city = city)
 
 
+# EDIT
+# GET '/cities/<id>/edit'
+@cities_blueprint.route("/cities/<id>/edit", methods=['GET'])
+def edit_cities(id):
+    city = city_repository.select(id)
+    countries = country_repository.select_all()
+    return render_template("cities/edit.html", city = city, all_countries = countries)
+
+# UPDATE
+# PUT '/cities/<id>'
+@cities_blueprint.route("/cities/<id>", methods=['POST'])
+def update_city(id):
+    name = request.form["name"]
+    city_type = request.form["city_type"]
+    country = country_repository.select(request.form["country_id"])
+    city = City (name, city_type, country, id)
+    print(city.country.name())
+    city_repository.update(city)
+    return redirect('/books')
+
+# DELETE
+# DELETE '/books/<id>'
+@cities_blueprint.route("/cities/<id>/delete", methods=['POST'])
+def delete_city(id):
+    city_repository.delete(id)
+    return redirect('/cities')   
+
+
 
 
 
